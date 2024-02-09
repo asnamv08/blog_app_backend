f4a054a2921a550721a571857c4ae844d62b20bf
asnamv08 <asnamvarghese001@gmail.com>
2024-02-09 11:00:11 +0530

    updated

--- controllers/blogroute.js
+++ controllers/blogroute.js
@@ -3,10 +3,17 @@ const signupmodel=require("../models/usermodel")
 const router=express.Router()
 
 router.post("/add",async(req,res)=>{
-    let data=req.body
-    let blog=new signupmodel(data)
-    let result= await blog.save()
+    let name=req.body.name
+    let age=req.body.age
+    let phone=req.body.phone
+    let address=req.body.address
+    let pincode=req.body.pincode
+    let email=req.body.email
+    let password=req.body.password
+    //let blog=new signupmodel(data)
+    //let result= await blog.save()
     res.json({
         status:"success"
     })
 })
+module.exports=router
